781065fd6d7461eeae83cb73080665064d5d550f
kavingam <bhaitigam71@gmail.com>
2024-04-05 16:52:00 +0530

    update functions code in program-1.cpp file

--- linklist/program-1.cpp
+++ linklist/program-1.cpp
@@ -2,12 +2,57 @@
 using namespace std;
 struct node {
     int data;
-    node *head;
+    node *next;
 };
-class {
-
+class single{
+    private: 
+        node *head;
+    public:
+        single(){
+            head = NULL;
+        }
+        /**/
+        void display(){
+            node *ptr = head;
+            if(head == NULL){
+                cout<<"empty list"<<endl;
+                return;
+            }
+            cout<<"list is: ";
+            while(ptr!=NULL){
+                cout<<ptr->data<<" ";
+                ptr=ptr->next;
+            }
+            cout<<endl;
+        }
+        /*function addbeg() adds a node at the beggining of the list*/ 
+        void addbeg(int data){
+            node *ptr = new node;
+            ptr->data = data;
+            ptr->next = head;
+            head = ptr;
+        }
+        /*function addend() insert a node at the end of the list*/
+        void addend(int data){
+            node *ptr = new node;
+            ptr->data = data;
+            node *tmp = head;
+            while(tmp->next != NULL){
+                tmp = tmp->next;
+            }
+            tmp->next = ptr;
+            ptr->next = NULL;
+        }
+        /*function addafter() insert a new node at the after the node */
+        void addafter(int data,int item){
+            node *tmp,*ptr = new node;
+        }
 };
 int main() {
-
+    single obj;
+    obj.addbeg(1);
+    obj.addbeg(2);
+    // obj.addend(3);
+    obj.display();
     return 0;
 }
