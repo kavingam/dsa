b45d98d547588a1ad6b391b2b9841457c088947f
kavingam <bhaitigam71@gmail.com>
2024-04-08 18:17:23 +0530

    update program-2.cpp

--- linklist/program-2.cpp
+++ linklist/program-2.cpp
@@ -2,12 +2,26 @@
 using namespace std;
 struct node {
     int data;
-    node *head;
+    node *prev;
+    node *next;
 };
-class {
+class DoubleLinkList{
 
 };
 int main() {
+    node *first_node = new node();
+    first_node->data = 1;
+    first_node->prev = NULL;
+    
+    node *second_node = new node();
+    second_node->data = 2;
+    second_node->prev = first_node;
+    second_node->next = NULL;
+    first_node->next = second_node;
 
+    while(first_node != NULL){
+        cout<<first_node->data<<" ";
+        first_node = first_node->next;
+    }
     return 0;
 }
