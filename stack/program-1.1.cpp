6b08e00ab4b83439c7fbf6a2cd387fb984910e44
kavingam <bhaitigam71@gmail.com>
2024-04-09 16:18:43 +0530

    update code

---
+++ stack/program-1.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+
+int main() {
+  std::stack<int> s;
+  s.push(10);
+  s.push(20);
+  s.push(30);
+
+  std::cout << s.top() << std::endl; // Output: 30
+  s.pop();
+
+  std::cout << s.top() << std::endl; // Output: 20
+
+  return 0;
+}
